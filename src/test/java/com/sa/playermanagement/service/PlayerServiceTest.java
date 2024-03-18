62d39167b660215213e31f8b198eef8a467f2782
sa5282 <shariq.akhmed@gmail.com>
2024-03-18 00:29:06 -0400

    Minor fixes to Pom.xml and added a README file

--- src/test/java/com/sa/playermanagement/service/PlayerServiceTest.java
+++ src/test/java/com/sa/playermanagement/service/PlayerServiceTest.java
@@ -4,24 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sa.playermanagement.db.model.Player;
 
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class PlayerServiceTest {
+class PlayerServiceTest {
 	
 	@Autowired
 	private PlayerService playerService;
 
 	@Test
-	public void test1_findByGendercodeLevelAndAge() {
+	void test1_findByGendercodeLevelAndAge() {
 		
 		List<Player> players = playerService.findByGenderLevelAndAge("M", 9, 25);
 
@@ -29,7 +25,7 @@ public class PlayerServiceTest {
 	}
 	
 	@Test
-	public void test2_findByGendercodeLevelAndAge() {
+	void test2_findByGendercodeLevelAndAge() {
 		
 		List<Player> players = playerService.findByGenderLevelAndAge("F", 10, 31);
 		System.out.println("Test 2 player size : " + players.size());
--- src/test/java/com/sa/playermanagement/service/SportServiceTests.java
+++ src/test/java/com/sa/playermanagement/service/SportServiceTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
